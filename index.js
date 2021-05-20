736007225e85f4ec44577d13501dbe45e9ad2861
Mark Manipula <markmikomanipula@gmail.com>
2021-05-20 09:13:02 -0700

    updated the put functionality. its better now!

--- index.js
+++ index.js
@@ -58,24 +58,56 @@ app.delete("/destination/:id", (req, res) => {
       res.send(`Item id:${id} is deleted`)
 })
 
+//THIS IS THE WRONG WAY TO UPDATE!
 //update data. using an id, update its destination, location, url, and description
-app.put("/destination/:id/:destination/:location/:url/:description", (req, res) => {
+// app.put("/destination/:id/:destination/:location/:url/:description", (req, res) => {
 
-      const { id, destination, location, url, description } = req.params
+//       const { id, destination, location, url, description } = req.params
 
-      //look for the id in the array using a loop
-      for (let i = 0; i < list.length; i++) {
-            if (list[i].id == id) {
-                  //i wonder if i can deconstructure this
-                  list[i].destination = destination;
-                  list[i].location = location;
-                  list[i].url = url;
-                  list[i].description = description;
-                  //so we dont keep looking at the array because
-                  //we can only modify one at a time. for now
-                  break;
+//       //look for the id in the array using a loop
+//       for (let i = 0; i < list.length; i++) {
+//             const listItem = list[i]
+//             if (listItem.id == id) {
+//                   //i wonder if i can deconstructure this
+//                   listItem.destination = destination;
+//                   listItem.location = location;
+//                   listItem.url = url;
+//                   listItem.description = description;
+//                   //so we dont keep looking at the array because
+//                   //we can only modify one at a time. for now
+//                   break;
+//             }
+//       }
+
+//       res.send(`Item id:${id} is updated!`)
+// })
+
+//working on update on class
+app.put("/destination/:id", (req, res) => {
+      const { id } = req.params
+      const { destination, location, url, description } = req.body
+
+      if (!destination && !location && !url && !description) {
+            return res.status(400).json({ error: "Must update at least one property" })
+      }
+
+      //trying the fancy loop
+      for (let item of list) {
+
+            if (item.id == id) {
+                  //update given destination, locaiton, url, description
+                  if (destination) item.destination = destination
+                  if (location) item.location = location
+                  if (url) item.url = url
+                  if (description) item.description = description
+                  //break out of the loop because we're only updating one
+                  break
             }
       }
 
       res.send(`Item id:${id} is updated!`)
+
 })
+
+
+//how to make http/api request in node?
